1e3dd9e43a909d32a05b379fba17822b598cd08e
alchemistmatt <matthew.monroe@pnnl.gov>
2015-05-22 14:14:33 -0700

    Add StartStopEventArgs.cs (from LCMS git)

---
+++ BuzzardLib/Searching/StartStopEventArgs.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace BuzzardLib.Searching
+{
+    public class StartStopEventArgs : EventArgs
+    {
+        public StartStopEventArgs(bool monitoring)
+        {
+            Monitoring = monitoring;
+        }
+
+        /// <summary>
+        /// True when monitoring
+        /// </summary>
+        public bool Monitoring
+        {
+            get;
+            private set;
+        }
+    }
+}
