294d31cfd33a3f886c17cf33d83a2f0d2fcb97c0
alchemistmatt <matthew.monroe@pnnl.gov>
2017-03-06 11:21:21 -0800

    Add First Chance Exception handling

--- BuzzardWPF/App.xaml.cs
+++ BuzzardWPF/App.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System.Windows;
+﻿using System;
+using System.Windows;
 using BuzzardWPF.Windows;
 using LcmsNetDataClasses.Logging;
 using LcmsNetDmsTools;
@@ -15,6 +16,8 @@ namespace BuzzardWPF
         private void Application_Startup(object sender, StartupEventArgs e)
         {
 
+            AppDomain.CurrentDomain.FirstChanceException += CurrentDomain_FirstChanceException;
+
             DynamicSplashScreen = new DynamicSplash
             {
                 Title = "Buzzard",
@@ -22,11 +25,32 @@ namespace BuzzardWPF
             };
             DynamicSplashScreen.Show();
             DynamicSplashScreen.InitializeInBackground();
+
+            
         }
 
         private void Application_DispatcherUnhandledException(object sender, System.Windows.Threading.DispatcherUnhandledExceptionEventArgs e)
         {
             classApplicationLogger.LogError(0, "Buzzard had an unhandled error.  " + e.Exception.Message, e.Exception);
         }
+
+        private void CurrentDomain_FirstChanceException(object sender, System.Runtime.ExceptionServices.FirstChanceExceptionEventArgs e)
+        {
+            if (e.Exception is System.IO.IOException && e.Exception.Message.Contains("logo_2017"))
+            {
+                // Exception is "Cannot locate resource 'resources/logo_2017.png'."
+                // It can safely be ignored
+                return;
+            }
+
+            if (e.Exception is System.IO.FileNotFoundException && e.Exception.Message.Contains("System.XmlSerializers"))
+            {
+                // Exception is "Could not load file or assembly 'System.XmlSerializers, ... The system cannot find the file specified."
+                // It can safely be ignored (see http://stackoverflow.com/questions/1127431/xmlserializer-giving-filenotfoundexception-at-constructor)
+                return;
+            }
+            classApplicationLogger.LogError(0, "Buzzard had an unhandled error.  " + e.Exception.Message, e.Exception);
+        }
+
     }
 }
