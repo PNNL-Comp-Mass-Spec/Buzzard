87ade64da76d4ba2194b0a213e6b96c2fec43e25
Bryson Gibbons <bryson.gibbons@pnnl.gov>
2019-05-09 15:25:49 -0700

    Fix design-time display errors with the DatasetsDataGridView

---
+++ BuzzardWPF/Views/DatasetBindingProxy.cs
@@ -0,0 +1,32 @@
+﻿using System.Windows;
+using BuzzardWPF.ViewModels;
+using WpfExtras;
+
+namespace BuzzardWPF.Views
+{
+    /// <summary>
+    /// Binding proxy to help with DataGrid binding to base DataContext
+    /// </summary>
+    /// <remarks>https://thomaslevesque.com/2011/03/21/wpf-how-to-bind-to-data-when-the-datacontext-is-not-inherited/</remarks>
+    public class DatasetBindingProxy : BindingProxy<DatasetsViewModel>
+    {
+        protected override BindingProxy<DatasetsViewModel> CreateNewInstance()
+        {
+            return new DatasetBindingProxy();
+        }
+
+        /// <summary>
+        /// Data object for binding
+        /// </summary>
+        public override DatasetsViewModel Data
+        {
+            get => (DatasetsViewModel)GetValue(DataProperty);
+            set => SetValue(DataProperty, value);
+        }
+
+        /// <summary>
+        /// DependencyProperty definition for Data
+        /// </summary>
+        public new static readonly DependencyProperty DataProperty = BindingProxy<DatasetsViewModel>.DataProperty.AddOwner(typeof(DatasetBindingProxy));
+    }
+}
