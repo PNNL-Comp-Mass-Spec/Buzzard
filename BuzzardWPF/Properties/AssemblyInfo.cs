96a774564c5a804a46565a3f3bf53f9e2efbd9f0
Bryson Gibbons <bryson.gibbons@pnnl.gov>
2020-01-21 23:15:40 -0800

    Update LCMSNet DLLs and NuGet Packages; bump version
    Primary change is a reduced number of users to use for providing an instrument operator (according to DMS permissions)

--- BuzzardWPF/Properties/AssemblyInfo.cs
+++ BuzzardWPF/Properties/AssemblyInfo.cs
@@ -49,6 +49,6 @@ using BuzzardWPF;
 // the build version of the running program to that at \\proto-5\BionetSoftware\Buzzard
 //
 // Innosetup reads this version from file BuzzardWPF.exe when building the installer (see Buzzard_x86-WPF.iss)
-[assembly: AssemblyVersion("2.0.0.0")]
-[assembly: AssemblyDate("January 16, 2020")]
+[assembly: AssemblyVersion("2.0.1.0")]
+[assembly: AssemblyDate("January 21, 2020")]
 
