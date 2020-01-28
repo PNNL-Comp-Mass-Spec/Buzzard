721609e68e9f0b4f91c8b14c2cfc40a82aecebc5
Bryson Gibbons <bryson.gibbons@pnnl.gov>
2020-01-28 10:54:18 -0800

    Update LCMSNet DLLs and bump version
    Updated LCMSNet DLLs have general string deduplication on database reads, and also reduced object size for reading requested runs

---
+++ BuzzardWPF/Management/RequestedRun.cs
@@ -0,0 +1,18 @@
+﻿using LcmsNetData.Data;
+
+namespace BuzzardWPF.Management
+{
+    /// <summary>
+    /// RequestedRun data container
+    /// </summary>
+    public class RequestedRun : IRequestedRunData
+    {
+        public RequestedRun()
+        {
+            DmsData = new DMSData();
+        }
+
+        /// <inheritdoc cref="IRequestedRunData"/>>
+        public DMSData DmsData { get; set; }
+    }
+}
