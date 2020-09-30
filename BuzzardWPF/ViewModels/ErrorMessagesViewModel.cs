df1811cff67c7527ffaa5770ca2056dbc825fc1e
alchemistmatt <matthew.monroe@pnnl.gov>
2020-09-29 17:56:54 -0700

    Add a window to show error messages

---
+++ BuzzardWPF/ViewModels/ErrorMessagesViewModel.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Input;
+using ReactiveUI;
+
+namespace BuzzardWPF.ViewModels
+{
+    public class ErrorMessagesViewModel : ReactiveObject
+    {
+        #region Attributes
+
+        private List<string> errorMessages;
+
+        #endregion
+
+        #region Initialization
+
+        public ErrorMessagesViewModel()
+        {
+            errorMessages = new List<string>();
+        }
+
+        #endregion
+
+        #region Properties
+
+        public List<string> ErrorMessages
+        {
+            get => errorMessages;
+            set => this.RaiseAndSetIfChanged(ref errorMessages, value);
+        }
+
+        #endregion
+
+    }
+}
---
+++ BuzzardWPF/Views/ErrorMessagesView.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace BuzzardWPF.Views
+{
+    /// <summary>
+    /// Interaction logic for ErrorMessagesView.xaml
+    /// </summary>
+    public partial class ErrorMessagesView : Window
+    {
+        public ErrorMessagesView()
+        {
+            this.
+            InitializeComponent();
+        }
+
+        private void Close_OnClick(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+
+    }
+}
