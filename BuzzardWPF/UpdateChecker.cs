75ce5e89d115eefa0c05e98350908c85ed8aa3a9
Bryson Gibbons <bryson.gibbons@pnnl.gov>
2021-02-02 01:14:39 -0800

    Update checks: Check if current user is an administrator, and warn if they are not.

--- BuzzardWPF/UpdateChecker.cs
+++ BuzzardWPF/UpdateChecker.cs
@@ -1,11 +1,10 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Diagnostics;
 using System.IO;
 using System.Linq;
 using System.Reflection;
-using System.Text;
-using System.Threading.Tasks;
+using System.Runtime.InteropServices;
+using System.Security.Principal;
 using System.Windows;
 using LcmsNetData;
 using LcmsNetData.Logging;
@@ -31,6 +30,12 @@ namespace BuzzardWPF
             {
                 var updateMsg = "A new version of Buzzard is available at " + update.InstallerFolderPath + "; Install the new version now?";
                 updateMsg += $"\n\nCurrent Version:\t{update.RunningVersion}\nNew Version:\t{update.InstallerVersionText}";
+                var currentUserIsAnAdministrator = UserAdminHelper.IsUserAnAdministrator();
+
+                if (!currentUserIsAnAdministrator)
+                {
+                    updateMsg += $"\n\nWARNING: Current user '{Environment.UserName}' is not an administrator and cannot install the update.\nSelecting 'Yes' will prompt for an administrator login!";
+                }
 
                 MessageBoxResult eResponse;
                 if (displayWindow != null)
@@ -204,6 +209,117 @@ namespace BuzzardWPF
             {
                 ApplicationLogger.LogMessage(0, "Error launching the installer for the new version (" + localInstallerPath + "): " + ex.Message);
                 System.Threading.Thread.Sleep(750);
+            }
+        }
+
+        public static class UserAdminHelper
+        {
+            // Simplest solution to properly do this...
+            // http://www.sadrobot.co.nz/blog/2011/06/20/how-to-check-if-the-current-user-is-an-administrator-even-if-uac-is-on/
+
+            [DllImport("advapi32.dll", SetLastError = true)]
+            static extern bool GetTokenInformation(IntPtr tokenHandle, TokenInformationClass tokenInformationClass, IntPtr tokenInformation, int tokenInformationLength, out int returnLength);
+
+            /// <summary>
+            /// Passed to <see cref="GetTokenInformation"/> to specify what
+            /// information about the token to return.
+            /// </summary>
+            enum TokenInformationClass
+            {
+                TokenUser = 1,
+                TokenGroups,
+                TokenPrivileges,
+                TokenOwner,
+                TokenPrimaryGroup,
+                TokenDefaultDacl,
+                TokenSource,
+                TokenType,
+                TokenImpersonationLevel,
+                TokenStatistics,
+                TokenRestrictedSids,
+                TokenSessionId,
+                TokenGroupsAndPrivileges,
+                TokenSessionReference,
+                TokenSandBoxInert,
+                TokenAuditPolicy,
+                TokenOrigin,
+                TokenElevationType,
+                TokenLinkedToken,
+                TokenElevation,
+                TokenHasRestrictions,
+                TokenAccessInformation,
+                TokenVirtualizationAllowed,
+                TokenVirtualizationEnabled,
+                TokenIntegrityLevel,
+                TokenUiAccess,
+                TokenMandatoryPolicy,
+                TokenLogonSid,
+                MaxTokenInfoClass
+            }
+
+            /// <summary>
+            /// The elevation type for a user token.
+            /// </summary>
+            enum TokenElevationType
+            {
+                TokenElevationTypeDefault = 1,
+                TokenElevationTypeFull,
+                TokenElevationTypeLimited
+            }
+
+            public static bool IsUserAnAdministrator()
+            {
+                var identity = WindowsIdentity.GetCurrent();
+                if (identity == null) throw new InvalidOperationException("Couldn't get the current user identity");
+                var principal = new WindowsPrincipal(identity);
+
+                // Check if this user has the Administrator role. If they do, return immediately.
+                // If UAC is on, and the process is not elevated, then this will actually return false.
+                if (principal.IsInRole(WindowsBuiltInRole.Administrator)) return true;
+
+                // If we're not running in Vista onwards, we don't have to worry about checking for UAC.
+                if (Environment.OSVersion.Platform != PlatformID.Win32NT || Environment.OSVersion.Version.Major < 6)
+                {
+                    // Operating system does not support UAC; skipping elevation check.
+                    return false;
+                }
+
+                int tokenInfLength = Marshal.SizeOf(typeof(int));
+                IntPtr tokenInformation = Marshal.AllocHGlobal(tokenInfLength);
+
+                try
+                {
+                    var token = identity.Token;
+                    var result = GetTokenInformation(token, TokenInformationClass.TokenElevationType, tokenInformation, tokenInfLength, out tokenInfLength);
+
+                    if (!result)
+                    {
+                        var exception = Marshal.GetExceptionForHR(Marshal.GetHRForLastWin32Error());
+                        throw new InvalidOperationException("Couldn't get token information", exception);
+                    }
+
+                    var elevationType = (TokenElevationType)Marshal.ReadInt32(tokenInformation);
+
+                    switch (elevationType)
+                    {
+                        case TokenElevationType.TokenElevationTypeDefault:
+                            // TokenElevationTypeDefault - User is not using a split token, so they cannot elevate.
+                            return false;
+                        case TokenElevationType.TokenElevationTypeFull:
+                            // TokenElevationTypeFull - User has a split token, and the process is running elevated. Assuming they're an administrator.
+                            return true;
+                        case TokenElevationType.TokenElevationTypeLimited:
+                            // TokenElevationTypeLimited - User has a split token, but the process is not running elevated. Assuming they're an administrator.
+                            return true;
+                        default:
+                            // Unknown token elevation type.
+                            return false;
+                    }
+                }
+                finally
+                {
+                    if (tokenInformation != IntPtr.Zero) Marshal.FreeHGlobal(tokenInformation);
+                }
             }
         }
     }
