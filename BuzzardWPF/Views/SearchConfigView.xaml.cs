28cdc2420d1f7de3d1282846270ab1b66f0a4024
Bryson Gibbons <bryson.gibbons@pnnl.gov>
2023-09-22 14:54:20 -0700

    Add a hacky fix to the AutoCompleteBox so that it can actually work.

--- BuzzardWPF/Views/SearchConfigView.xaml.cs
+++ BuzzardWPF/Views/SearchConfigView.xaml.cs
@@ -11,5 +11,26 @@ namespace BuzzardWPF.Views
         {
             InitializeComponent();
         }
+
+        private string lastSelection = "";
+
+        private void AutoCompleteBox_OnSelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            if (!(sender is AutoCompleteBox acb))
+            {
+                return;
+            }
+
+            // HACK!!: The AutoCompleteBox, for some reason, will do auto-complete when you select an entry, and then immediately revert it
+            // This code is to cache the selected value, and then (when the selected item is removed because it was 'applied') re-apply the selected value
+            if (e.AddedItems.Count > 0)
+            {
+                lastSelection = e.AddedItems[0].ToString();
+            }
+            else if (!string.IsNullOrWhiteSpace(lastSelection))
+            {
+                acb.Text = lastSelection;
+            }
+        }
     }
 }
